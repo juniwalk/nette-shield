86a95c042d9003af6e70841c287b8c7e6c34dbb7
Martin Procházka <juniwalk@outlook.cz>
2015-01-15 11:10:36 +0100

    DEL: Removed ability to output files
    Ability to output files uisng ShieldAction::setOutput was removed, use
    ShieldAction::invokeCallback instead.

--- src/ShieldAction.php
+++ src/ShieldAction.php
@@ -90,23 +90,11 @@ class ShieldAction
      * Redirect to given url
      * 
      * @param  Shield  $shield  Instance of Shield
-     * @param  string  $data    Data/File to be outputed
+     * @param  string  $data    Data to be outputed
      * @throws ErrorException
      */
     public function setOutput(Shield $shield, $data)
     {
-        // If given path is a file and it is readable
-        if (is_file($data) && is_readable($data)) {
-            // Load its data into the holder
-            $data = file_get_contents($data);
-        }
-
-        // If the data is object and there is to string converter method
-        if (is_object($data) && method_exists($data, '__toString')) {
-            // Convert object into string
-            $data = $data->__toString();
-        }
-
         // If the data value is not scalar except null
         if (!is_scalar($data) && !is_null($data)) {
             throw new \ErrorException('Shield: Only scalar values can be outputed.');
