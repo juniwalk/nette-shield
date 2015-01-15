d617c64355000a484a6aec245d054071906a1260
Martin Procházka <juniwalk@outlook.cz>
2015-01-15 08:50:37 +0100

    ADD: ShieldAction class
    Added class which will manage all the actions in shield library.

---
+++ src/ShieldAction.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   Shield
+ * @link      https://github.com/juniwalk/shield
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Shield;
+
+class ShieldAction
+{
+    
+}
