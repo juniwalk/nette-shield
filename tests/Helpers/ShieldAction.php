c4c62e11a5f0c1b818e1834645378ff3398af5d0
Martin Procházka <juniwalk@outlook.cz>
2015-01-17 00:06:06 +0100

    ADD: ShieldAction mock class for testing

---
+++ tests/Helpers/ShieldAction.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   Shield
+ * @link      https://github.com/juniwalk/shield
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Shield\Tests\Helpers;
+
+/**
+ * Extend ShieldAction class and override ShieldAction::headers_sent()
+ * to return true, instead of calling headers_sent().
+ */
+class ShieldAction extends \JuniWalk\Shield\ShieldAction
+{
+    /**
+     * Are headers already sent out?
+     *
+     * @param  string  $file  Path to file
+     * @param  int     $line  Line number
+     * @return bool
+     */
+    protected static function headers_sent(&$file = null, &$line = null)
+    {
+        // Just some values for testing
+        $file = __FILE__;
+        $line = __LINE__;
+
+        return true;
+    }
+}
